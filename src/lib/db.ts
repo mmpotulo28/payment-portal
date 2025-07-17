68cc327388c8dd4ef9aa5ce44ea3f308986b5aa3
Manelisi Mpotulo <mpotulom28@gmail.com>
2025-07-17 15:48:20 +0200

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-	process.env.EXPO_PUBLIC_SUPABASE_URL!,
-	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 	{
 		auth: {
 			persistSession: true,
@@ -12,8 +12,8 @@ const supabase = createClient(
 );
 
 const supabaseAdmin = createClient(
-	process.env.EXPO_PUBLIC_SUPABASE_URL!,
-	process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
 );
 
 export { supabaseAdmin };
